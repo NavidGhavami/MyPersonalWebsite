ae4b125069844fc9f6100f7b257a4d36463f368f
Navid Ghavami <navid_ghavami@outlook.com>
2020-12-03 17:28:56 +0330

    aboutMe Models added to project

---
+++ MyPersonalWebsite/MyPersonalWebsite/Models/AboutMe.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPersonalWebsite.Models
+{
+    public class AboutMe
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class Service
+    {
+        public string ServiceTitle { get; set; }
+        public string ServiceDescription { get; set; }
+    }
+
+    public class PriceTable
+    {
+        public string PriceTableTitle { get; set; }
+        public string Price { get; set; }
+        public string PriceTableDescription { get; set; }
+    }
+    public class Ability
+    {
+        public string AbilityTitle { get; set; }
+    }
+    public class Customers
+    {
+        public string CustomersTitle { get; set; }
+    }
+
+
+}
