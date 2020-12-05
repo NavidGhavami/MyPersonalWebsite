c95534e221e383adc179e5a5d981f0a8cffd5e7c
Navid Ghavami <navid_ghavami@outlook.com>
2020-12-06 00:27:43 +0330

    Articles and ContactMe and Projects view Component  added to project

---
+++ MyPersonalWebsite/MyPersonalWebsite/ViewComponents/ContactMeViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyPersonalWebsite.Models;
+
+namespace MyPersonalWebsite.ViewComponents
+{
+    public class ContactMeViewComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var contact = new ContactMe(
+                1,
+                "ایران . تهران . میدان ونک . خیابان ونک . پلاک 12",
+                "navid_ghavami@outlook.com",
+                "8585 787 0933",
+                "شنبه تا پنج شنبه ساعت 8 الی 21");
+
+
+            return View("_ContactMe", contact);
+        }
+    }
+}
---
+++ MyPersonalWebsite/MyPersonalWebsite/ViewComponents/ProjectViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyPersonalWebsite.Models;
+
+namespace MyPersonalWebsite.ViewComponents
+{
+    public class ProjectViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var project = new List<Projects>
+            {
+                new Projects(1,"Antomi","فروشگاهی","antomi.png"),
+                new Projects(2,"Blogxer","خبری","blogxer.png"),
+            };
+            return View("_Project", project);
+        }
+    }
+}
